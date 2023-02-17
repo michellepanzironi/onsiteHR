be28e93f47d113812590a0569d3fec3a84f40f75
Michelle Panzironi <michelle.panzironi@gmail.com>
2023-02-17 14:42:44 -0500

    set up useOnClickOutside hook

--- src/Modal.js
+++ src/Modal.js
@@ -1,27 +1,33 @@
 import React, { Fragment } from 'react';
 import ReactDOM from 'react-dom';
+import useOnClickOutside from './hooks/useOnClickOutside'
 
-const Modal = ({ isOpen, hide }) => isOpen ? ReactDOM.createPortal(
-	<Fragment>
-		<div className="modal-overlay" />
-		<div className="modal-wrap" aria-modal aria-hidden tabIndex={-1} role="dialog">
-			<div className="modal">
-				<div className="modal-header">
-					<button 
-						className="modal-close-button"
-						onClick={hide}
-						type="button"
-						arai-label="close"
-					>
-						<span aria-hidden="true">&times;</span>
-					</button>
-				</div>
-				<div className='modal-body'>
-					modal body
+const Modal = ({ isOpen, hide }) => {
+	const ref = React.useRef();
+	useOnClickOutside(ref, hide);
+
+	return isOpen ? ReactDOM.createPortal(
+		<Fragment>
+			<div className="modal-overlay" />
+			<div className="modal-wrap" aria-modal aria-hidden tabIndex={-1} role="dialog">
+				<div className="modal" ref={ref}>
+					<div className="modal-header">
+						<button 
+							className="modal-close-button"
+							onClick={hide}
+							type="button"
+							arai-label="close"
+						>
+							<span aria-hidden="true">&times;</span>
+						</button>
+					</div>
+					<div className='modal-body'>
+						modal body
+					</div>
 				</div>
 			</div>
-		</div>
-	</Fragment>, document.body
-) : null;
+		</Fragment>, document.body
+	) : null;
+}
 
 export default Modal;
